f7ebf6851896ff76ac9e80d660cd83b9f6a8b741
Haechan Mark Bong <haechan.bong@gmail.com>
2023-08-29 01:38:28 -0400

    Update clip_interrogator.py

--- clip_interrogator/clip_interrogator.py
+++ clip_interrogator/clip_interrogator.py
@@ -212,9 +212,9 @@ class Interrogator():
         env = ", ".join(self.envs.rank(image_features, max_envs))
 
         if caption.startswith(res):
-            prompt = f"A {frame} of {positive} {{}} in {env}"
+            prompt = f"A {positive} {frame} of {{}} in {env}"
         else:
-            prompt = f"A {res} {frame} of {positive} {{}} in {env}"
+            prompt = f"A{res} {positive} {frame} of {{}} in {env}"
 
         return _truncate_to_fit(prompt, self.tokenize)
 
